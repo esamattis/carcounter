ae8e6457408065208f721f1561d32766793f2f3f
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-04-13 14:34:56 +0300

    Load es5shim if Function.prototype.bind is missing

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,19 @@
+// Preloading entry point for our app
 
 var $script = require("scriptjs").$script;
 
-window.USE_ZEPTO = !!document.querySelectorAll;
+// Scripts to load
+var scripts = [];
 
-if (window.USE_ZEPTO) $script("bundle/main-zepto.js");
-else $script("bundle/main-jquery.js");
+// If Function.prototype.bind is missing load es5-shim
+if (!Function.prototype.bind) scripts.push("scripts/vendor/es5-shim.js");
+
+// if we have querySelectorAll we can safely use Zepto version of the bundle
+window.USE_ZEPTO = !!document.querySelectorAll;
+if (window.USE_ZEPTO) scripts.push("bundle/main-zepto.js");
+else scripts.push("bundle/main-jquery.js");
+
+// Load both scripts in parallel. We don't need to use callback here because
+// the main bundles are entry points. They will be executed soon as they are
+// loaded or after adding es5-shim.
+$script(scripts);
